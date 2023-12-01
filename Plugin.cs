7202bab36b295dc3b5719e706204c0ed6c169c60
NotSoEpic <legotomsmith@gmail.com>
2023-12-01 20:33:18 +0800

    this is such a fuckin mess, but now it works?

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using PersistentPurchases;
@@ -6,40 +7,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace PersistentPurchases
 {
     [BepInPlugin("beeisyou.PersistentPurchases", "Persistent Purchases", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        private static ConfigEntry<bool> resetSuits;
+        private static ConfigEntry<bool> resetFurniture;
+        private static ConfigEntry<bool> resetUpgrades;
+        private static ConfigEntry<bool> defaultPlacement;
+
         public static ManualLogSource log = new ManualLogSource("Persistent Purchases");
         public static Harmony harmony = new Harmony("beeisyou.PersistentPurchases");
         private void Awake()
         {
             BepInEx.Logging.Logger.Sources.Add(log);
 
+            resetSuits = Config.Bind("Resetting", "ResetSuits", false, "Remove all but the orange suit on game over");
+            resetFurniture = Config.Bind("Resetting", "ResetFurniture", false, "Remove cosmetic purchases on game over");
+            resetUpgrades = Config.Bind("Resetting", "ResetUpgrades", true, "Remove ship upgrades on game over");
+            defaultPlacement = Config.Bind("Resetting", "DefaultPlacement", true, "Reset objects to their default position, and put everything else in storage");
+
             harmony.PatchAll(typeof(Patches));
             harmony.PatchAll(typeof(ResetSavedGameValues));
             harmony.PatchAll(typeof(ResetShip));
+            harmony.PatchAll(typeof(InSanityCheck));
 
             log.LogMessage("Plugin Persistent Purchases is loaded!");
         }
 
         public static int[] suits = { 0, 1, 2, 3 }; // orange, green, yellow, pajamas
         public static int[] upgrades = { 5, 18, 19 }; // teleporter, horn, inv teleporter
+        public static int[] defaults = { 7, 8, 11, 15, 16 }; // cupboard, cabinet, light switch, bunkbeds, terminal
 
-        public static bool shouldReset(int id, string debugType)
+        public static bool shouldReset(int id, string debugType="")
         {
-            if (suits.Contains(id))
+            if (suits.Contains(id) && !resetSuits.Value)
             {
                 return false;
             }
-            if (upgrades.Contains(id))
+            else if (upgrades.Contains(id) && !resetUpgrades.Value)
             {
-                return true;
+                return false;
             }
-            return false;
+            else if (!resetFurniture.Value)
+            {
+                return false;
+            }
+            if (defaults.Contains(id))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool shouldDefault()
+        {
+            return defaultPlacement.Value;
         }
     }
 }
@@ -47,7 +75,7 @@ namespace PersistentPurchases
 [HarmonyPatch]
 public class Patches
 {
-    [HarmonyPrefix, HarmonyPatch(typeof(GameNetworkManager), nameof(GameNetworkManager.ResetUnlockablesListValues))]
+    /*[HarmonyPrefix, HarmonyPatch(typeof(GameNetworkManager), nameof(GameNetworkManager.ResetUnlockablesListValues))]
     public static bool dontResetAnything()
     {
         if (StartOfRound.Instance != null)
@@ -58,6 +86,7 @@ public class Patches
             {
                 if (Plugin.shouldReset(i, "dontResetAnything")) // the only modified line lol
                 {
+                    Plugin.log.LogInfo($"Resetting {list[i].unlockableName}");
                     list[i].hasBeenUnlockedByPlayer = false;
                     if (list[i].unlockableType == 1)
                     {
@@ -70,7 +99,7 @@ public class Patches
             }
         }
         return false; // skip original function
-    }
+    }*/
 
 
     [HarmonyPostfix, HarmonyPatch(typeof(GameNetworkManager), nameof(GameNetworkManager.ResetSavedGameValues))]
@@ -102,16 +131,67 @@ public class Patches
         PlaceableShipObject[] array = UnityEngine.Object.FindObjectsOfType<PlaceableShipObject>();
         for (int i = 0; i < array.Length; i++)
         {
-            if (array[i].parentObject == null)
+            UnlockableItem item = StartOfRound.Instance.unlockablesList.unlockables[array[i].unlockableID];
+            if (item.spawnPrefab)
             {
-                Debug.Log("Error! No parentObject for placeable object: " + StartOfRound.Instance.unlockablesList.unlockables[array[i].unlockableID].unlockableName);
-            }
-            if (StartOfRound.Instance.unlockablesList.unlockables[array[i].unlockableID].spawnPrefab)
-            {
+                item.inStorage = true;
                 Collider[] componentsInChildren = array[i].parentObject.GetComponentsInChildren<Collider>();
                 for (int j = 0; j < componentsInChildren.Length; j++)
                 {
                     componentsInChildren[j].enabled = true;
+                }
+            }
+            else if(Plugin.shouldDefault())
+            {
+                if (item.alreadyUnlocked)
+                {
+                    item.inStorage = false;
+                    array[i].parentObject.disableObject = false;
+                    ShipBuildModeManager.Instance.ResetShipObjectToDefaultPosition(array[i]);
+                }
+                else
+                {
+                    if (Plugin.shouldReset(array[i].unlockableID))
+                    {
+                        item.hasBeenUnlockedByPlayer = true;
+                        item.inStorage = true;
+                    } 
+                    else
+                    {
+                        item.hasBeenUnlockedByPlayer = false;
+                        item.inStorage = false;
+                    }
+                    array[i].parentObject.disableObject = true;
+                    ShipBuildModeManager.Instance.StoreObjectServerRpc(
+                        array[i].parentObject.GetComponent<NetworkObject>(), 
+                        (int)GameNetworkManager.Instance.localPlayerController.playerClientId
+                    );
+                }
+            }
+        }
+
+        for (int i = 0; i < StartOfRound.Instance.unlockablesList.unlockables.Count; i++)
+        {
+            UnlockableItem item = StartOfRound.Instance.unlockablesList.unlockables[i];
+            if (!item.alreadyUnlocked && item.spawnPrefab)
+            {
+                GameObject gameObject;
+                if (!StartOfRound.Instance.SpawnedShipUnlockables.TryGetValue(i, out gameObject))
+                {
+                    StartOfRound.Instance.SpawnedShipUnlockables.Remove(i);
+                }
+                else if (gameObject == null)
+                {
+                    StartOfRound.Instance.SpawnedShipUnlockables.Remove(i);
+                }
+                else
+                {
+                    StartOfRound.Instance.SpawnedShipUnlockables.Remove(i);
+                    NetworkObject component = gameObject.GetComponent<NetworkObject>();
+                    if (component != null && component.IsSpawned)
+                    {
+                        component.Despawn(true);
+                    }
                 }
             }
         }
@@ -211,7 +291,7 @@ public class ResetShip {
                 {
                     prefabI2 = i + 2;
                     prefabJ2 = codes[i + 1].Clone();
-                    prefabJ2.opcode = OpCodes.Br;
+                    prefabJ2.opcode = OpCodes.Brfalse_S;
                     Plugin.log.LogInfo($"Found ship object prefab opcode at {i} (1)");
                 }
             }
@@ -234,7 +314,10 @@ public class ResetShip {
 
         if (prefabI2 >= 0)
         {
-            codes.Insert(prefabI2, prefabJ2);
+            codes.InsertRange(prefabI2, new CodeInstruction[] {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Plugin), nameof(Plugin.shouldDefault))),
+                prefabJ2
+            });
         }
 
         if (prefabI1 >= 0)
@@ -249,5 +332,49 @@ public class ResetShip {
         }
 
         return codes.AsEnumerable();
+    }
+}
+
+[HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.SyncShipUnlockablesServerRpc))]
+public class InSanityCheck
+{
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        Plugin.log.LogWarning("Beginning transpilation of StartOfRound.SyncShipUnlockablesServerRpc()");
+        var codes = new List<CodeInstruction>(instructions);
+
+        for (int i = 0; i < codes.Count; i++)
+        {
+            if (codes[i].opcode == OpCodes.Ldstr && codes[i].operand.ToString() == "Server: placeableObject #{0}: {1}")
+            {
+                codes.InsertRange(i - 1, new CodeInstruction[] 
+                {
+                    new CodeInstruction(OpCodes.Ldloc_S, 5),
+                    new CodeInstruction(OpCodes.Ldloc_S, 10),
+                    new CodeInstruction(OpCodes.Ldelem_Ref),
+                    new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(InSanityCheck), nameof(WhatIsGoingOn))) 
+                }
+                );
+                Plugin.log.LogWarning("This crash is not evading me");
+                break;
+            }
+        }
+
+        return codes.AsEnumerable();
+    }
+
+    public static void WhatIsGoingOn(PlaceableShipObject pso)
+    {
+        Plugin.log.LogWarning("Seeing what the fuck is going wrong here with syncing unlockables, prepare for spam");
+        Plugin.log.LogInfo($"ship object is not null? {pso != null}");
+        Plugin.log.LogInfo($"ship object transform is not null? {pso.transform != null}");
+        int id = pso.unlockableID;
+        Plugin.log.LogInfo($"ship object unlockable id {id}");
+        Plugin.log.LogInfo($"unlockables has id? {id < StartOfRound.Instance.unlockablesList.unlockables.Count}");
+        UnlockableItem item = StartOfRound.Instance.unlockablesList.unlockables[id];
+        Plugin.log.LogInfo($"unlockable is not null? {item != null}");
+        Plugin.log.LogInfo($"unlockable position {item.placedPosition}");
+        Plugin.log.LogInfo($"unlockable rotation {item.placedPosition}");
+        Plugin.log.LogInfo($"unlockable in storage? {item.inStorage}");
     }
 }
